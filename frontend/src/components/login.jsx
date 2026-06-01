58bf9e7e381f68921ceeaf699b6c6b75f680abad
avivlkeshet-dev <avivlkeshet@gmail.com>
2026-06-01 16:42:43 +0300

    Fixed login

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,3 +1,4 @@
+
 import './login.css';
 
 function img() {
--- frontend/src/components/loginInfo.jsx
+++ frontend/src/components/loginInfo.jsx
@@ -3,7 +3,6 @@ import './loginInfo.css'
 function LoginInfo({ infoText, placeholderText }) {
     return (
         <div className="login-container">
-            <p> hi</p>
             <p className="login-text">{infoText}</p>
             <input type="text" placeholder={placeholderText} />
         </div>
